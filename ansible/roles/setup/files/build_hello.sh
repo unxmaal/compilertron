a8bd345e7529adcea8a02e78bf5ea37bed3e97ae
Eric Dodd <eric.e.dodd@gmail.com>
2018-12-31 20:05:59 -0500

    initial testing

---
+++ ansible/roles/setup/files/build_hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=/opt/binutils/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+main(){
+    cd /opt/scratch
+    /opt/gcc/bin/mips-sgi-irix6.5-gcc hello.c
+}
+
+main 
+exit $?
